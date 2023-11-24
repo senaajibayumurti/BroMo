949a7e5995d3002dd8d959b124ced7ecd4eb02b2
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-11-24 13:35:06 +0700

    edit kandang-switch background when switching

--- public/js/kandang-switch.js
+++ public/js/kandang-switch.js
@@ -2,7 +2,9 @@ var btn = document.getElementById('switch-bg')
 
 function leftClick(){
     btn.style.left = '0'
+    btn.style.background = '#3AB46A'
 }
 function rightClick(){
     btn.style.left = '110px'
+    btn.style.background = '#FEB618'
 }
