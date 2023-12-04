9f08c9ef3d7be0706a1e1a9fcc7716a1a1ab41c7
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-12-04 22:56:49 +0700

    Finished doughnut Chart

--- resources/views/owner/forecasting.blade.php
+++ resources/views/owner/forecasting.blade.php
@@ -1,27 +1,22 @@
 <x-app-layout>
     <link rel="stylesheet" href="/css/forecasting.css">
+    <link rel="stylesheet" href="/css/chart.css">
 
     @push('navbar-title')
         <span class="bm-font-clr1 bm-font-bold3 bm-font-36">FORECASTING</span>
     @endpush
     <div id="info" class="d-flex flex-column justify-content-around h-100">
-        <div class="d-flex flex-row justify-content-between h-25">
+        <div class="d-flex flex-row justify-content-between h-auto">
             <div id="card" class="d-flex flex-column justify-content-between align-items-center py-2 w-25">
-                <div>
-                    <canvas id="forecasting-chart-1"></canvas>
-                </div>
+                <div class="pie" >25%</div>
                 <div class="bm-font-24 bm-font-semibold">Suhu</div>
             </div>
             <div id="card" class="d-flex flex-column justify-content-between align-items-center py-2 w-25">
-                <div>
-                    <canvas id="forecasting-chart-2"></canvas>
-                </div>
+                <div class="pie" style="--p:50">50%</div>
                 <div class="bm-font-24 bm-font-semibold">Kelembaban</div>
             </div>
             <div id="card" class="d-flex flex-column justify-content-between align-items-center py-2 w-25">
-                <div>
-                    <canvas id="forecasting-chart-3"></canvas>
-                </div>
+                <div class="pie" style="--p:100">100%</div>
                 <div class="bm-font-24 bm-font-semibold">Amonia</div>
             </div>
         </div>
@@ -53,5 +48,4 @@
             </div>
         </div>
     </div>
-    <script src="/js/chart.js"></script>
 </x-app-layout>
